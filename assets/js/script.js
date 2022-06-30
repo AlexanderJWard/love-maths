32bbac99693725c0203262d73893c6573b8c1bb2
AlexanderJWard <alexjamesward@hotmail.com>
2022-06-30 14:13:54 +0000

    Display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,11 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`)
+                runGame(gameType);
             }
         })
     }
+    runGame("addition");
 })
 
 /**
@@ -17,9 +18,16 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after a user's answer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unkown game type: ${gameType}`)
+        throw `Unkown game type: ${gameType}. Aborting!`
+    }
 }
 
 function checkAnswer() {
@@ -38,8 +46,10 @@ function incrementWrongAnswer() {
     
 }
 
-function displayAdditionQuestion() {
-    
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
